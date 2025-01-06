272ad1c263eb6b1ea4e800cb3f227aa65156c59b
Ali Najafi <mail.ali.najafi@gmail.com>
2025-01-06 15:22:23 +0400

    Enhance AmazonAEParser: Support HTML as string or etree.Element, add methods for date first available and best sellers rank extraction

--- AmazonParser/parser.py
+++ AmazonParser/parser.py
@@ -1,17 +1,31 @@
+from typing import Union
 from lxml import etree
 import re
 from pprint import pprint
+from datetime import datetime
 
 class Parser:
-    def __init__(self, html, base_url=None):
+    def __init__(self, html: Union[str, etree._Element], base_url=None):
+        """ Parser class
+            Support html as a text or prtial etree._Element for nested usecase
+            BASE_URL uses for create full URL in <a> tags
+        """    
+        if isinstance(html, str):
+            self.html = html
+        else:
+            self.html = str(etree.tostring(html))
+        
         self.base_url = base_url
-        self.html = html
-        self.metadata = self.extract_metadata(html)
+        self.metadata = self.extract_metadata(self.html)
 
         # Base URL
         if not self.base_url and 'BASE_URL' in self.metadata:
             self.base_url = self.metadata['BASE_URL']
-        self.tree = etree.HTML(html, base_url=self.base_url)
+
+        if isinstance(html, str):
+            self.tree = etree.HTML(html, base_url=self.base_url)
+        else:
+            self.tree = html
 
     @staticmethod
     def get_html_from_file(path):
@@ -35,16 +49,16 @@ class Parser:
         if xpath.endswith('//text()'):
             result = self.get_elements_or_none(xpath)
             if result:
-                return ' '.join([item.strip() for item in result])
-
+                result = ' '.join([item.strip() for item in result])
         # Get Element
-        result = self.get_elements_or_none(xpath, max_num_result=1)
-
-        # Prepair Result
-        if result:
-            result = result[0].strip()
         else:
-            return None
+            result = self.get_elements_or_none(xpath, max_num_result=1)
+            if result:
+                result = result[0]
+                if isinstance(result, str):
+                    result = result.strip()
+            else:
+                return None
 
         # Regex
         if regex:
@@ -63,8 +77,15 @@ class Parser:
             return None
         
         if max_num_result and len(result) > max_num_result:
-            return result[:max_num_result]
-        return result
+            result = result[:max_num_result]
+        
+        final_result = []
+        for r in result:
+            if isinstance(r, etree._Element):
+                final_result.append(Parser(html=r, base_url=self.base_url))
+            else:
+                final_result.append(r)
+        return final_result
 
     def get_full_url(self, partial_url):
         """ Return BASE_URL/PARTIAL_URL """
@@ -77,7 +98,8 @@ class Parser:
 
 class AmazonAEParser(Parser):
     def get_product_details(self):
-        """ Collect Product Details """
+        """ Collect Product Details
+        """
         return {
             'title': self.get_title(),
             'brand': self.get_brand_name(),
@@ -87,15 +109,20 @@ class AmazonAEParser(Parser):
             'bought_past_mounth': self.get_bought_past_mounth(),
             'customers_reviews': self.get_reviews(),
             'bullet_points': self.get_bullet_points(),
+            'date_first_available': self.get_date_first_available(),
+            'best_sellers_rank': self.get_best_sellers_rank(),
         }
     
     def get_title(self):
-        """ Extract Title """
-        return self.get_element_or_none('//*[@id="productTitle"]/text()').strip()
+        """ Extract Title
+        """
+        return self.get_element_or_none('//*[@id="productTitle"]/text()')
     
     def get_price(self):
-        """ Extract Price and Currency """
+        """ Extract Price and Currency
+        """
         res = self.get_element_or_none('//span[@id="tp_price_block_total_price_ww"]//span[@class="a-offscreen"]/text()')
+        print(res)
         if res:
             res = res.strip()
             price = float(res.split(' ')[1])
@@ -107,7 +134,8 @@ class AmazonAEParser(Parser):
         }
     
     def get_image(self):
-        """ Extract Image URL """
+        """ Extract Image URL
+        """
         landing_img = self.get_element_or_none('//*[@id="landingImage"]/@src')
         original_size_img = self.get_element_or_none('//img[@id="landingImage"]/@data-old-hires')
 
@@ -116,14 +144,16 @@ class AmazonAEParser(Parser):
         return landing_img
     
     def get_brand_name(self):
-        """ Extract Brand Name """
+        """ Extract Brand Name
+        """
         text = self.get_element_or_none('//*[@id="bylineInfo"]/text()', r"Visit the (.+) Store")
         if text:
             text = text.strip()
         return text
             
     def get_seller_detail(self):
-        """ Extract Seller Details """
+        """ Extract Seller Details
+        """
         seller_name = self.get_element_or_none('//a[@id="sellerProfileTriggerId"]/text()')
         if seller_name:
             seller_name = seller_name.strip()
@@ -138,7 +168,8 @@ class AmazonAEParser(Parser):
         }
     
     def get_bought_past_mounth(self):
-        """ Extract Sales Details """
+        """ Extract Sales Details
+        """
         xpath = '//div[@data-feature-name="socialProofingAsinFaceout"]//span[@class="a-text-bold"]/text()'
         regex = r'([\d\+]+) bought'
         bought_past_mounth = self.get_element_or_none(xpath, regex)
@@ -146,12 +177,14 @@ class AmazonAEParser(Parser):
     
 
     def get_bullet_points(self):
-        """ Extract Bullet Points """
+        """ Extract Bullet Points
+        """
         result = self.get_elements_or_none('//div[@id="feature-bullets"]/ul//li//text()')
         return '\n'.join([item.strip() for item in result])
     
     def get_reviews(self):
-        """ Extract Reviews """
+        """ Extract Reviews
+        """
         # Rate
         review_rate = self.get_element_or_none('//span[@id="acrPopover"]/@title', r'([\d\.]+) out of 5 stars')
         if review_rate:
@@ -167,3 +200,36 @@ class AmazonAEParser(Parser):
         }
     
     
+    def get_date_first_available(self):
+        """ Extract 'Date First Available' and return as a Python Date format
+        """
+        result = self.get_element_or_none('//th[contains(text(), "Date First Available")]/../td/text()')
+        if result:
+            result = datetime.strptime(result, '%d %B %Y').date()
+        return result
+    
+
+    def get_best_sellers_rank(self):
+        """ Extract Best Sellers Rank
+        """
+        result = self.get_elements_or_none('//th[contains(text(), "Best Sellers Rank")]/../td/span/span')
+        if not result:
+            return None
+        
+        ranks = []
+        for r in result:
+            rank = {
+                'rank': None,
+                'category': None,
+                'category_url': None,
+            }
+            found = r.get_element_or_none('.//text()', regex=r'#([\d]+) in ([\w\s&]+)')
+            if found:
+                rank['rank'] = int(found[0])
+                rank['category'] = found[1].strip()
+                
+                url = r.get_element_or_none('./a/@href')
+                url = self.get_full_url(partial_url=url)
+                rank['category_url'] = url
+                ranks.append(rank)
+        return ranks
