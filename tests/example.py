3e3ad679565e7c28fa405a06ba2ed05084f2c368
Ali Najafi <mail.ali.najafi@gmail.com>
2025-01-06 15:23:02 +0400

    sample code to use AmazonParser

--- tests/example.py
+++ tests/example.py
@@ -1,11 +1,25 @@
 import sys
 sys.path.insert(0, '../AmazonParser')
-from AmazonParser import AmazonParser
+from AmazonParser import AmazonAEParser, Parser
+import glob
 
 
 if __name__ == '__main__':
-    html = AmazonParser.get_html_from_file('tests/archives/B0D83GHDW2.html') # Read HTML file 
-    p = AmazonParser(html=html) # Create an instance of AmazonParser
-    
-    for key, value in p.metadata.items():
-        print(f'{key}: {value}')
+    for path in glob.glob('tests/archives/*.html'):
+        print(path)
+        html = AmazonAEParser.get_html_from_file(path)
+        p = AmazonAEParser(html=html, base_url="https://www.amazon.ae/") # Create an instance of AmazonAEParser
+        
+        print('\nMetadata:')
+        for key, value in p.metadata.items():
+            print(f'{key}: {value}')
+        
+        print('\nPage Detail:')
+        for key, value in p.get_product_details().items():
+            print(f'{key}: {value}')
+
+        result: list[Parser] = p.get_best_sellers_rank()
+        print(result)
+        
+        break
+
