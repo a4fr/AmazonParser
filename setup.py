5f8e5a83b6aa3215ecb51489df66cc5364ce96ca
Ali Najafi <mail.ali.najafi@gmail.com>
2025-01-06 10:01:29 +0400

    Update version to test Action

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="AmazonParser",
-    version="0.1.0",
+    version="0.1.2",
     author="Ali Najafi",
     author_email="mail.ali.najafi@gmail.com",
     description="A Parser for Amazon Pages",
